f400d3a656b3f0df99272dc013961cc1930f1503
michal-kubicki <michal-kubicki@users.noreply.github.com>
2019-07-20 19:49:49 +0100

    Removed hardcoded number of CPU/Cores

--- New-Generic-VM.ps1
+++ New-Generic-VM.ps1
@@ -11,24 +11,24 @@ param(
     
     [parameter(Mandatory=$true)]
     [ValidateRange(1, [int64]::MaxValue)]
-    [int64] $VMProcessorCount,
+    [int64]$VMProcessorCount,
 
     [Parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path $_})]
     [string]$ISOPath
 )
 
-#Set the switch name, HDD and memory size, number of CPU
+#Config section:
+#Set the switch name, HDD and memory size, number of CPUs
 $VMSwitchName = 'External Virtual Switch'
 $VHDXSizeBytes = 10GB
 $MemoryStartupBytes = 2GB
-$VMProcessorCount = 2
 
 #Get the default HDD path
 $vmmsSettings = Get-WmiObject -namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData
 $vhdxPath = Join-Path $vmmsSettings.DefaultVirtualHardDiskPath "$VMName.vhdx"
 
-#Create new Hyper-V VM
+#Create a new Hyper-V VM
 New-VM -Name $VMName -Generation 2 -SwitchName $VMSwitchName -MemoryStartupBytes $MemoryStartupBytes -NewVHDPath $vhdxPath -NewVHDSizeBytes $VHDXSizeBytes
 
 #VM Configure the new VM
