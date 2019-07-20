322d018912c4496b46db988c383f6f16674910cd
michal-kubicki <michal-kubicki@users.noreply.github.com>
2019-07-20 17:28:11 +0100

    First commit

---
+++ New-Generic-VM.ps1
@@ -0,0 +1,37 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding()]
+
+#Let's gather some important information
+param(
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$VMName,
+    
+    [parameter(Mandatory=$true)]
+    [ValidateRange(1, [int64]::MaxValue)]
+    [int64] $VMProcessorCount,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ISOPath
+)
+
+#Set the switch name, HDD and memory size, number of CPU
+$VMSwitchName = 'External Virtual Switch'
+$VHDXSizeBytes = 10GB
+$MemoryStartupBytes = 2GB
+$VMProcessorCount = 2
+
+#Get the default HDD path
+$vmms = gwmi -namespace root\virtualization\v2 Msvm_VirtualSystemManagementService
+$vmmsSettings = gwmi -namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData
+$vhdxPath = Join-Path $vmmsSettings.DefaultVirtualHardDiskPath "$VMName.vhdx"
+
+#Create new Hyper-V VM
+New-VM -Name $VMName -Generation 2 -SwitchName  $VMSwitchName -MemoryStartupBytes $MemoryStartupBytes -NewVHDPath $vhdxPath -NewVHDSizeBytes $VHDXSizeBytes
+
+#VM Configure the new VM
+Set-VMProcessor $VMName -Count $VMProcessorCount
+Set-VMFirmware -VMName $VMName -EnableSecureBoot off
